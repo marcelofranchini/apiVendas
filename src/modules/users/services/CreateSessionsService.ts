42e6b80b45c69e627f8d6935d06962f488f91d30
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-07-23 19:09:57 -0300

    update

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,45 @@
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import { sign } from 'jsonwebtoken';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+      const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password combination.', 401);
+      }
+    
+      const token = sign({}, '1980079f11a39b8c53aba66e1d607388', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default CreateSessionsService;
