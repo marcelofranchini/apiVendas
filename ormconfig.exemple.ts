2497129fcb1e86f15a6812f373aa55e9ef18a87a
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-08-04 23:51:44 -0300

    s3 aws upload, raterlimiter, classTransformer

--- ormconfig.exemple.ts
+++ ormconfig.exemple.ts
@@ -1,13 +1,13 @@
 module.exports = {
-    type: ,
+    type: 'postgres',
     host: process.env.DB_HOST,
-    port: ,
+    port: 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    migrations: ['src/shared/typeorm/migrations/*.ts'],
-    entities: ['src/modules/**/typeorm/entities/*.ts'],
+    migrations: ['dist/shared/typeorm/migrations/*.js'],
+    entities: ['dist/modules/**/typeorm/entities/*.js'],
     cli: {
-        migrationsDir: './src/shared/typeorm/migrations',
+        migrationsDir: './dist/shared/typeorm/migrations',
     },
 };
